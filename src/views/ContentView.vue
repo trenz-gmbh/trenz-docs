86494270bd562bbbe69bef98343081cedf39690f
Ricardo Boss <r.boss@trenz.de>
2022-07-08 13:01:43 +0200

    Prism code highlighting and marked markdown rendering

--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -1,33 +1,92 @@
 <template>
-	<v-container>
-		<vue-markdown :source="content"></vue-markdown>
-	</v-container>
+  <v-container v-html="output"></v-container>
 </template>
+
+<style lang="scss">
+@import "prismjs/themes/prism-coy.min.css";
+
+ol, ul {
+  margin-left: 1rem;
+}
+
+$code-color: #c92c2c;
+$code-background-color: #eeeeee;
+
+code:not(pre > code) {
+  background: $code-background-color;
+  color: $code-color;
+  border-radius: 0.25rem;
+  padding: 0 0.25rem;
+}
+
+code[class*=language-] {
+  overflow: initial;
+}
+
+pre {
+  border: solid 1px #00000044;
+  overflow-x: auto;
+  padding: 0.5rem;
+}
+</style>
 
 <script lang="ts">
 import {defineComponent} from "vue";
 import {MeiliSearch} from "meilisearch";
-import VueMarkdown from "vue-markdown-render";
+import {marked} from 'marked';
+import Prism from 'prismjs';
+import 'prismjs/components/prism-clike.min.js'
+import 'prismjs/components/prism-yaml.min.js'
+import 'prismjs/components/prism-aspnet.min.js'
+import 'prismjs/components/prism-bash.min.js'
+import 'prismjs/components/prism-sql.min.js'
+import 'prismjs/components/prism-csharp.min.js'
+import 'prismjs/components/prism-json.min.js'
+import 'prismjs/components/prism-toml.min.js'
 
 export default defineComponent({
-	name: "ContentView",
+  name: "ContentView",
 
-	components: {
-		VueMarkdown,
-	},
+  props: {
+    location: {
+      type: String,
+      required: true,
+    },
+  },
 
-	props: {
-		location: {
-			type: String,
-			required: true,
-		},
-	},
+  data() {
+    return {
+      content: "Loading...",
+      options: {
+        gfm: true,
+        breaks: true, // requires gfm to be true
+        mangle: true,
+        smartypants: true,
+        highlight(code: string, lang: string, callback?: (error: unknown, code?: string) => void): string | void {
+          try {
+            let html;
+            if (!lang) {
+              html = code;
+            } else {
+              html = Prism.highlight(code, Prism.languages[lang], lang);
+            }
 
-	data() {
-		return {
-			content: "Loading...",
-		};
-	},
+            if (callback) callback(null, html);
+            else return html;
+          } catch (e) {
+            if (callback) callback(e);
+            else throw e;
+          }
+        }
+      } as marked.MarkedOptions,
+    };
+  },
+
+  computed: {
+    output() {
+      return marked(this.content, this.options);
+    }
+  },
 
   async beforeMount() {
     const client = new MeiliSearch({
@@ -35,20 +94,20 @@ export default defineComponent({
       apiKey: 'masterKey',
     })
 
-		const docs = await client.index('files').search("", {
-			filter: 'location = "' + this.location + '"',
-			limit: 1,
-			attributesToRetrieve: ["content"],
-		});
+    const docs = await client.index('files').search("", {
+      filter: 'location = "' + this.location + '"',
+      limit: 1,
+      attributesToRetrieve: ["content"],
+    });
 
-		if (docs.hits.length != 1) {
-			this.content = "#Not found\r\n\r\nThis page does not exist.";
+    if (docs.hits.length != 1) {
+      this.content = "# Not found\r\n\r\nThis page does not exist.";
 
-			return;
-		}
+      return;
+    }
 
-		const doc = docs.hits[0];
-		this.content = doc.content;
-	},
+    const doc = docs.hits[0];
+    this.content = doc.content;
+  },
 })
 </script>
