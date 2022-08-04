0c4948b21bd6beb3f8b8266666c1f646f5980e6a
Ricardo Boss <mail@ricardoboss.de>
2022-08-04 14:58:54 +0200

    Added HTTPS options to dev server

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,34 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+const {defineConfig} = require('@vue/cli-service')
+const fs = require('fs')
 
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
+let https = false
+let serverPort = 8080
+let serverOpts = {}
+if (process.env.CERT_PATH && process.env.CERT_PASSPHRASE) {
+    https = true
+    serverPort = 443
+    serverOpts = {
+        type: 'https',
+        options: {
+            pfx: fs.readFileSync(process.env.CERT_PATH),
+            passphrase: process.env.CERT_PASSPHRASE,
+        },
+    }
+}
+
+module.exports = defineConfig({
+    transpileDependencies: true,
+
+    pluginOptions: {
+        vuetify: {
+            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+        }
+    },
+
+    devServer: {
+        https,
+        port: serverPort,
+        allowedHosts: 'all',
+        server: serverOpts,
+    },
 })
