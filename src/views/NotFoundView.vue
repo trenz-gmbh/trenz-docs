701f2bfa3f301f38777836abc3f161e318c6e146
Ricardo Boss <mail@ricardoboss.de>
2022-08-04 14:44:23 +0200

    Add message informing user they may have no permission to view the page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,6 +1,38 @@
 <template>
-  <v-alert :prominent="true" type="error" variant="outlined">
-    This page does not exist.
+  <v-alert
+    type="error"
+    variant="tonal"
+    title="Not available"
+  >
+    <div>
+      We can't find that page. This could be because:
+
+      <ul>
+        <li>
+          The page doesn't exist.
+        </li>
+        <li>
+          The page exists, but you don't have view permission for it.
+        </li>
+      </ul>
+    </div>
+    <v-divider class="my-4 bg-red-lighten-4"></v-divider>
+    <div class="d-flex flex-row align-center justify-end">
+      <v-btn
+        class="mr-3"
+        variant="outlined"
+        @click="$router.back()"
+      >
+        Go back
+      </v-btn>
+
+      <v-btn
+        variant="outlined"
+        @click="$router.push('/')"
+      >
+        Go home
+      </v-btn>
+    </div>
   </v-alert>
 </template>
 
