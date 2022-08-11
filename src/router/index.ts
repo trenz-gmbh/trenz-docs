0646162a36ed45eb77a572c3ea72d5c2514b2403
Ricardo Boss <mail@ricardoboss.de>
2022-08-11 13:20:14 +0200

    Added dynamic title based on route

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,40 +3,52 @@ import ContentView from "@/views/ContentView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: '/',
-        name: 'home',
-        component: () => import(/* webpackChunkName: "home" */ '../views/HomeView.vue'),
-        meta: {
-            title: 'Home',
-        }
-    },
-    {
-        path: '/wiki/:location([^?#]*)',
-        name: 'content',
-        component: ContentView,
-        props: true,
-    },
-    {
-        path: '/search',
-        name: 'search',
-        component: () => import(/* webpackChunkName: "search" */ '../views/SearchView.vue'),
-        meta: {
-            title: 'Search',
-        }
-    },
-    {
-        path: '/:path(.*)*',
-        component: NotFoundView,
-        meta: {
-            title: '',
-        }
+  {
+    path: '/',
+    name: 'home',
+    component: () => import(/* webpackChunkName: "home" */ '../views/HomeView.vue'),
+    meta: {
+      title: 'Home',
     }
+  },
+  {
+    path: '/wiki/:location([^?#]*)',
+    name: 'content',
+    component: ContentView,
+    props: true,
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: () => import(/* webpackChunkName: "search" */ '../views/SearchView.vue'),
+    meta: {
+      title: 'Search',
+    }
+  },
+  {
+    path: '/:path(.*)*',
+    component: NotFoundView,
+    meta: {
+      title: 'Not Found',
+    }
+  }
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title + ' \u2022 ' + process.env.VUE_APP_NAME;
+  } else if (to.params.location) {
+    document.title = (to.params.location as string).split('/').pop() + ' \u2022 ' + process.env.VUE_APP_NAME;
+  } else {
+    document.title = process.env.VUE_APP_NAME;
+  }
+
+  next()
+});
 
 export default router
