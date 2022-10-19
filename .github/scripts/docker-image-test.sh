929bcc3fa0ce309de1d99eabc0e1ef464ec5acde
Ricardo Boss <contact@ricardoboss.de>
2022-10-19 12:33:31 +0200

    Also check received homepage

--- .github/scripts/docker-image-test.sh
+++ .github/scripts/docker-image-test.sh
@@ -15,7 +15,7 @@ requested_settings=$(curl http://localhost:5000/webapp-settings.json)
 # compare served file with actual file (a 404 or 50x error - or a timeout - would fail here)
 actual_settings=$(cat webapp-settings.local.json)
 if [ "$actual_settings" != "$requested_settings" ]; then
-  echo "Could not verify server functionality. Received:"
+  echo "Could not verify server functionality. Received settings:"
   echo "$requested_settings"
   exit 1
 fi
@@ -25,6 +25,12 @@ echo "$requested_settings"
 
 # test if homepage gets served without errors
 requested_homepage=$(curl http://localhost:5000/)
+actual_homepage=$(cat public/index.html)
+if [ "$actual_homepage" != "$requested_homepage" ]; then
+  echo "Could not verify server functionality. Received homepage:"
+  echo "$requested_settings"
+  exit 1
+fi
 
 echo "Received homepage:"
 echo "$requested_homepage"
